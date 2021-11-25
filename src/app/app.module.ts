75b365513832adef4e920ae3148d8cbbc378194a
Erick Cornejo <c.cornejo.erick@gmail.com>
2021-11-25 15:18:11 -0500

    component nav & nav-dashboard create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavPrincipalComponent } from './shared/nav-principal/nav-principal.component';
+import { NavDashboardComponent } from './shared/nav-dashboard/nav-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavPrincipalComponent,
+    NavDashboardComponent
   ],
   imports: [
     BrowserModule,
