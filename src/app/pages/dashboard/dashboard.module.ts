0be3ac20f2cd4e8e93458ca79b3bdb2c56f6ff2b
Alejo280102 <alejandrogutierrez@upeu.edu.pe>
2021-11-25 16:51:25 -0500

    components

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RecetasComponent } from './components/recetas/recetas.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RecetasComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
