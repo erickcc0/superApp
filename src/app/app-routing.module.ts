7e1c22fa25e528974477367104e4f7cb5ce755bd
Erick Cornejo <c.cornejo.erick@gmail.com>
2021-12-01 12:55:28 -0500

    ultimo minuto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './pages/graficos/table/table.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MainDetalleConsultorComponent } from './pages/detalle-consultor/main-detalle-consultor/main-detalle-consultor.component';
-
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent},
   { path: 'table', component: TableComponent},
   { path:'detalle', component:MainDetalleConsultorComponent},
-  { 
-    path: 'dashboard', 
-    loadChildren: () => 
-      import('./pages/dashboard/dashboard.module').then(
-        (m) => m.DashboardModule
-      ),
-  }
+  { path:'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  items: any[] = [
+    {
+      personas: 284,
+      categoria: 'Consultores'
+    },
+    {
+      personas: 11,
+      categoria: 'Vacantes'
+    },
+    {
+      personas: 21,
+      categoria: 'T.Remoto'
+    },
+    {
+      personas: '4%',
+      categoria: 'Rotacion'
+    },
+    {
+      personas: 37,
+      categoria: 'Permisos'
+    },
+    {
+      personas: 50,
+      categoria: 'V.Acomp'
+    }
+  ];
 
   constructor() { }
 
