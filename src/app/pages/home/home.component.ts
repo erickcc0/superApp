dd06720bc6e2c07f40adfd911c95a7936ec1cf6c
Alejo280102 <alejandrogutierrez@upeu.edu.pe>
2021-12-01 18:51:54 -0500

    endpoint correct

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
     getUser(): void {
       this._userService.getUser()
           .toPromise().then(
-            (response) => {
+            (response: any) => {
               this.users = response;
               console.table(this.users);
             }
