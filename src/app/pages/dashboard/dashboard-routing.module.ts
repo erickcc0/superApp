23c927c17b2e13b5cf37fafb00d5f59042933c25
Alejo280102 <alejandrogutierrez@upeu.edu.pe>
2021-12-06 11:42:33 -0500

    rebase complete again

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
-import {
-  RouterModule,
-  Routes,
-} from '@angular/router';
-
-import {
-  ResumenComponent,
-} from './components/recetas/resumen/resumen.component';
-import {
-  UseroptionsComponent,
-} from '../../shared/useroptions/useroptions.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ResumenComponent } from './components/recetas/resumen/resumen.component';
 
 const routes: Routes = [
-  { path: '', component: UseroptionsComponent },
-  { path: 'resumen', component: ResumenComponent }
+  { path: 'resumen', component: ResumenComponent}
 ];
 @NgModule({
+  declarations: [],
   imports: [
+    CommonModule,
     [RouterModule.forChild(routes)]
   ],
   exports: [RouterModule]
